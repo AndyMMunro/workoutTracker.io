ed1fb190c393368f7343c5afd040a21e56af5505
AndyMMunro <agovitadventures@gmail.com>
2020-11-09 12:56:22 -0800

    added update route and some action definitions

--- Develop/routes/workoutRoute.js
+++ Develop/routes/workoutRoute.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const workout = require("../models/workoutModel.js");
 
-
+// gets all workouts available in the data base
 router.get("/api/workouts", (req, res) => {
     workout.find({})
         .then(dbworkoutModel => {
@@ -13,26 +13,37 @@ router.get("/api/workouts", (req, res) => {
 });
 
 
+// creates data base and lets us push info to the database
 router.post("/api/workouts", (req, res) => {
-    Workout.create({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
+    workout.create({})
+        .then(dbworkoutModel => {
+            res.json(dbworkoutModel);
         })
         .catch(err => {
             res.json(err);
         });
 });
 
+router.post("/update/:id", (req, res) => {
+    workout.update({
+        _id: mongojs.ObjectId(req.params.id)
+    }, {
+        $set: {
+            type: req.body.type,
+            name: req.body.name,
+            duration: req.body.duration,
+            weight: req.body.weight,
+            reps: req.body.reps,
+            sets: req.body.sets
+        }
+    }, (err, data) => {
+        if (error) {
+            res.send(err);
+        } else {
+            res.send(data);
+        }
+    });
+});
 
-// app.get("/populateduser", (req, res) => {
-//     db.User.find({})
-//         .populate("notes")
-//         .then(dbUser => {
-//             res.json(dbUser);
-//         })
-//         .catch(err => {
-//             res.json(err);
-//         });
-// });
 
 module.exports = router
