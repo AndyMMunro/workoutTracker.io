7e34b7d4dcba30320f64522e213c0374f8536f10
AndyMMunro <agovitadventures@gmail.com>
2020-11-10 15:13:08 -0800

    added mongo deployment info into the server

--- server.js
+++ server.js
@@ -16,10 +16,19 @@ app.use(express.json());
 
 app.use(express.static("Develop/public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//     useNewUrlParser: true
+// });
 
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 // routes
 app.use(require("./Develop/routes/workoutRoute.js"));
 app.use(require("./Develop/routes/view.js"));
