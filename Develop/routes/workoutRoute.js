639a141ad14d9c5e59bac40a2ab5aadfb32e546d
AndyMMunro <agovitadventures@gmail.com>
2021-01-26 11:21:00 -0500

    changed the readme

--- Develop/routes/workoutRoute.js
+++ Develop/routes/workoutRoute.js
@@ -3,58 +3,54 @@ const Workout = require("../models/workoutModel.js");
 
 // creates new work out data set into the data base 
 router.post("/api/workouts", ({body}, res) => {
-                    Workout.create(
-                            body
-                        )
-                        .then(dbworkoutModel => {
-                            res.json(dbworkoutModel);
-                        })
-                        .catch(err => {
-                            res.json(err);
-                        });
-                });
+    Workout.create(body)
+        .then(dbworkoutModel => {res.json(dbworkoutModel);
+        })
+        .catch(err => { res.json(err);
+        });
+    });
 
 
-            // gets previous workout available to be displayed
-            router.get("/api/workouts", (req, res) => {
-                Workout.find({})
-                    .then(dbworkoutModel => {
-                        res.json(dbworkoutModel);
-                    })
-                    .catch(err => {
-                        res.json(err);
-                    });
-            });
+// gets previous workout available to be displayed
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbworkoutModel => {
+            res.json(dbworkoutModel);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 
 
-            // routes to show all of the workouts in the database
-            router.get("/api/workouts/range", (req, res) => {
-                Workout.find({})
-                    .then(dbworkoutModel => {
-                        res.json(dbworkoutModel);
-                    })
-                    .catch(err => {
-                        res.json(err);
-                    });
-            });
-            // adds new exercise to pervious
-            router.put("/api/workouts/:id", ({
-                body,
-                params
-            }, res) => {
-                Workout.findByIdAndUpdate(
-                        params.id, {
-                            $push: {
-                                exercises: body
-                            }
-                        }
-                    )
-                    .then(dbworkoutModel => {
-                        res.json(dbworkoutModel);
-                    })
-                    .catch(err => {
-                        res.json(err);
-                    });
-            });
+// routes to show all of the workouts in the database
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .then(dbworkoutModel => {
+            res.json(dbworkoutModel);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+// adds new exercise to pervious
+router.put("/api/workouts/:id", ({
+    body,
+    params
+}, res) => {
+    Workout.findByIdAndUpdate(
+            params.id, {
+                $push: {
+                    exercises: body
+                }
+            }
+        )
+        .then(dbworkoutModel => {
+            res.json(dbworkoutModel);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 
-            module.exports = router;
+module.exports = router;
--- README.md
+++ README.md
@@ -1,37 +1,40 @@
-# workoutTracker.io
-
 
   
-  # FitnessTracker
+  ## https://github.com/AndyMMunro/workoutTracker.io
   
-  #Full online mongodb based web app that allows the user to store workouts and add new wons and view them in charted progress based format. 
+  ## Description
+  Full online mongodb based web app that allows the user to store workouts and add new wons and view them in charted progress based format.
   
   ## Table Of Contents 
    
-  #Installation
-  #usage
-  #license 
-  #contributing
-  #test
-  #questions
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Questions](#questions)
+  * [Test](#test)
 
-  ##Installation
-  non
- 
-  ##Usage
-  entering in data relevent to your work out and submitting it
+  Installation:
+    Click on the link below and you will be directed to the live working version.
   
-  ##License 
+  gitHub: https://github.com/AndyMMunro/workoutTracker.io
+  
+  Live link: https://theworkout.herokuapp.com/?id=5fb6bf60b112bb0017a77ec0
+
+ 
+  Usage:
+    Keep track of workout information and see it charted and tracked and saved within a database. 
+  
+  License:
   MIT
   
-  #Contributing
+  Contributing:
   Andy M
   
-  #Test
-  non
+  Test:
+  NA
 
-## Questions 
+## Questions: 
 If you have any questions about the repo,
-open an issue or contact me directly at Andy M. 
-You can find more of my work at agovitadventures@gmail.com.
+open an issue or contact me directly at AndyMMunro. 
+You can find more of my work at andymunro87@gmail.com.
 
